c5b3b282becb9571e02dc2c4c7859c353bc5a1be
na0chan-go <nn19960218@gmail.com>
2024-09-11 21:18:59 +0900

    feat: サーバーストリーミングRPCのクライアント実装

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 
 	"github.com/na0chan-go/grpc-lesson/pb"
@@ -17,7 +18,8 @@ func main() {
 
 	c := pb.NewFileServiceClient(conn)
 	ctx := context.Background()
-	callListFiles(c, ctx)
+	// callListFiles(c, ctx)
+	callDownload(c, ctx)
 }
 
 func callListFiles(c pb.FileServiceClient, ctx context.Context) {
@@ -26,4 +28,25 @@ func callListFiles(c pb.FileServiceClient, ctx context.Context) {
 		log.Fatalf("Failed to call ListFiles: %v", err)
 	}
 	log.Printf("Response from ListFiles: %v", res.Filenames)
+}
+
+func callDownload(c pb.FileServiceClient, ctx context.Context) {
+	req := &pb.DownloadRequest{
+		Filename: "name.txt",
+	}
+	stream, err := c.Download(ctx, req)
+	if err != nil {
+		log.Fatalf("Failed to call Download: %v", err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to receive data: %v", err)
+		}
+		log.Printf("Response from Download(bytes): %v", res.GetData())
+		log.Printf("Response from Download(string): %v", string(res.GetData()))
+	}
 }
